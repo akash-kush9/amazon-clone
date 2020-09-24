69c5c511446e7ad00eed9a9204a869cc7684f9e4
akash-kush9 <akash.kushwaha9@gmail.com>
2020-09-24 17:02:23 +0530

    Display username on checkout page

--- src/Checkout/Checkout.js
+++ src/Checkout/Checkout.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { useStateValue } from "../StateProvider/StateProvider";
 import "./Checkout.css";
 import CheckoutProduct from "./CheckoutProduct/CheckoutProduct";
 import Subtotal from "./Subtotal/Subtotal";
-
 const Checkout = () => {
+  const [{ authuser }, dispatch] = useStateValue();
+
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -12,6 +14,9 @@ const Checkout = () => {
           src="https://images-eu.ssl-images-amazon.com/images/G/31/img15/home/LA/exchange_offers._V278112460_.jpg"
         />
         <div>
+          <h3 className="checkout__title">
+            Hello, {authuser?.displayName || authuser?.email.split("@")[0]}
+          </h3>
           <h2 className="checkout__title">Your Shopping Basket</h2>
         </div>
         <CheckoutProduct />
