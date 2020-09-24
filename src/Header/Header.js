30730d91b50f48eb9551d3f96e3675d64619bbd4
akash-kush9 <akash.kushwaha9@gmail.com>
2020-09-24 17:00:17 +0530

    Dynamic name of user added for display

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,9 +4,21 @@ import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
 import { useStateValue } from "../StateProvider/StateProvider";
-
+import * as actionTypes from "./../StateProvider/actionTypes";
+import { auth } from "./../firebase";
 const Header = () => {
   const [{ basket, authuser }, dispatch] = useStateValue();
+  // const signOut = (e) => {
+  //   e.preventDefault();
+  //   dispatch({
+  //     type: actionTypes.REMOVE_USER,
+  //   });
+  // };
+
+  const handleAuthentication = () => {
+    if (authuser) auth.signOut();
+  };
+
   return (
     <div className="header">
       <Link to="/">
@@ -24,17 +36,40 @@ const Header = () => {
         <SearchIcon className="header__searchIcon" />
       </div>
       <div className="header__nav">
-        <Link to="/login">
-          <div className="header__option">
+        <Link className="nav__link" to={!authuser && `/login`}>
+          <div onClick={handleAuthentication} className="header__option">
             <span className="header__optionLineOne">
-              Hello{" "}
-              {authuser ? ` ${authuser.user.email.split("@")[0]} ` : "Guest"}
+              {authuser
+                ? `Hello ${authuser?.email.split("@")[0]}`
+                : "Hello Guest"}
             </span>
             <span className="header__optionLineTwo">
               {authuser ? "Sign Out" : "Sign In"}
             </span>
           </div>
         </Link>
+
+        {/* <div className="header__nav">
+        <div className="header__option">
+          <span className="header__optionLineOne">
+            {authuser ? (
+              `Hello ${authuser?.email.split("@")[0]}`
+            ) : (
+              <Link className="nav__link" to="/login">
+                Hello Guest
+              </Link>
+            )}
+          </span>
+          <span className="header__optionLineTwo">
+            {authuser ? (
+              <div onClick={signOut}>Sign Out</div>
+            ) : (
+              <Link className="nav__link" to="/login">
+                Sign In
+              </Link>
+            )}
+          </span>
+        </div> */}
         <div className="header__option">
           <span className="header__optionLineOne">Returns </span>
           <span className="header__optionLineTwo">Orders</span>
