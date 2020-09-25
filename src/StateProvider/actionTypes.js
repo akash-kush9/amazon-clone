1e0dc0774811d820af2dacf8708e4e4b99c2e25a
akash-kush9 <akash.kushwaha9@gmail.com>
2020-09-25 23:56:44 +0530

    New actiontype entered for empty basket

--- src/StateProvider/actionTypes.js
+++ src/StateProvider/actionTypes.js
@@ -3,3 +3,4 @@ export const REMOVE_FROM_BASKET = "REMOVE_FROM_BASKET";
 export const UPDATE_TOTAL_PRICE = "UPDATE_TOTAL_PRICE";
 export const SET_USER = "SET_USER";
 export const REMOVE_USER = "REMOVE_USER";
+export const EMPTY_BASKET = "EMPTY_BASKET";
